e2e03b60467bc21e5c02214cdab81bc5e50dbd20
raulsandoval98 <112207515+raulsandoval98@users.noreply.github.com>
2025-03-04 16:36:03 -0600

    cambios inventario

--- app/Controllers/InventarioController.php
+++ app/Controllers/InventarioController.php
@@ -119,6 +119,10 @@ class InventarioController extends BaseController
 
     public function guardar()
     {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        
         $json = $this->request->getJSON();
         if ($json) {
             echo true;

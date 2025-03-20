c6ef3ec60e042a3e04a2b75b1d0602c53192c167
raulsandoval98 <112207515+raulsandoval98@users.noreply.github.com>
2025-03-20 09:51:41 -0600

    cambio login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -26,16 +26,22 @@ class Login extends BaseController
         // Call the buscarUsuario method to search for the user and verify the password
         $user = $UsuariosModel->buscarUsuario($username, $password);
 
-        if ($user) {
+        if ($user["correo"] == true && $user["password"] == true){
 
-            session()->set('user_id', $user->id);
+            session()->set('user_id', $user["id"]);
             session()->set('username', $username);
 
             return $this->response->setJSON(['status' => 'success']);
 
-        } else {
+        }
+        if ($user["correo"] == false && $user["password"] == false){
             
-            return $this->response->setJSON(['status' => 'invalid_password']);
+            return $this->response->setJSON(['status' => 'correo_invalido']);
+
+        }
+        if ($user["correo"] == true && $user["password"] == false){
+            
+            return $this->response->setJSON(['status' => 'password_invalido']);
 
         }
     }

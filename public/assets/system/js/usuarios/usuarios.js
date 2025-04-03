52a57a383160e8ce9a523333f8efbe0e27cfd06b
raulsandoval98 <112207515+raulsandoval98@users.noreply.github.com>
2025-04-03 17:54:18 -0600

    centrado de columna

--- public/assets/system/js/usuarios/usuarios.js
+++ public/assets/system/js/usuarios/usuarios.js
@@ -19,5 +19,13 @@ $('#tabla_usuarios').DataTable({
                 return '<button class="btn btn-primary"><i class="bx bx-show"></i> Ver</button>';
             }
         }
+    ],
+    columnDefs: [
+        {
+            targets: 0, // Aplica la regla a la primera columna (índice 0)
+            createdCell: function(td, cellData, rowData, row, col) {
+                $(td).css('text-align', 'left');
+            }
+        }
     ]
 });
